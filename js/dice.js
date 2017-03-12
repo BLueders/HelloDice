11307611d688a575099181de3f60cbe96087bca9
Benno <benno.lueders@gmail.com>
2017-03-12 19:01:29 +0100

    Debug die generation and basic logic for rolling dice

--- js/dice.js
+++ js/dice.js
@@ -2,19 +2,50 @@ $(function(){
 
 	var counter = 0;
 
+	$('.debug-setup-button').click(function(){
+		create_new_dice();
+		create_new_dice();
+		create_new_dice();
+		
+		create_new_side_row(1,0);
+		create_new_side_row(1,1);
+		create_new_side_row(2,0);
+		create_new_side_row(2,1);
+		create_new_side_row(2,2);
+		create_new_side_row(3,0);
+
+		$('.die-side-input').each(function(i){
+			$(this).val(i);
+		});
+		
+	});
+	
+	
+	
 	$('.add-die-button').click(function() {
 		create_new_dice();
-	})
+	});
+
+	$('.roll-dice-button').click(function() {
+		roll_the_dice();
+	});
 	
-	
+	$('.dice-container').on('change', '.sides-input', function(e) {
+		var value = $(this).val();
+		
+		if(value > 99){
+			$(this).val(99);
+		}
+	});
 	
 	$('.dice-container').on('keyup', '.sides-input', function(e) {
+		var value = $(this).val();
 		
-		if (e.keyCode == 13) {
-			var dieBox = $(this).parent('.die-box');
+		if (e.keyCode == 13) {			
+			var dieBox = $(this).parents('.die-box');
 			var dieID = dieBox.attr('data-die-id');
 			//dieBox.find('.die-sides-container').empty();
-			var value = $(this).val();
+	
 			for(var i=0;i<value;i++)
 			{
 				create_new_side_row(dieID, i);
@@ -22,7 +53,6 @@ $(function(){
 		    return false; // prevent the button click from happening
 		}
 	});
-	
 	
 	function create_new_dice()
 	{
@@ -41,23 +71,43 @@ $(function(){
 	
 	function create_new_side_row(die_id, index)
 	{
-		
-		var dieBox = $('.dice-container > .die-box-'+counter);
+		var dieBox = $('.dice-container > .die-box-'+die_id);
 		var newSideCopy =$('.die-side-row-template').clone();
 		newSideCopy.removeClass('die-side-row-template');
-		newSideCopy.appendTo(dieBox);
+		newSideCopy.appendTo(dieBox.find('.die-sides-container'));
 		//newSideCopy.appendTo(dieBox.find('.die-sides-container'));
 		newSideCopy.attr('data-side-id',index);
 		newSideCopy.find('.die-side-text').html(index);
-		console.log(newSideCopy);
+		//console.log(newSideCopy);
 	}
 	
+	function roll_the_dice(){
+		var diceContainer = $('.dice-container');
+		var diceElements = diceContainer.find('.die-box');
+		var dice = [];
+		diceElements.each(function(die){
+			dice[die] = new Die();
+			var sidesElements = $(this).find('.die-sides-container > .die-side-row');
+			sidesElements.each(function(side){
+				var inputField = $(this).find('.die-side-input');
+				dice[die].sides[side] = inputField.val();
+				console.log(inputField);
+				console.log(inputField.val());
+			});
+			// console.log(dice);
+		});
+	}
 });
 
 function Die(){
-	this.sites = [];
+	this.sides = [];
 }
 
 function DieSite(){
 	this.siteValue = 0;
 }
+
+
+// var errorDiv = document.getElementById("errorDiv"),
+                    // regex = /^[a-z0-9]+$/,
+                    // str = document.getElementById("inputString").value;
